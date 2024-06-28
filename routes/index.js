c4d152ff2a49439f03e9e40c984c7d9075195492
EER Team <eer.engine.22@gmail.com>
2024-06-28 21:43:20 +0700

    Test

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,20 @@ r.use('/demo', demo);
 
 r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
 
+r.get('/mock/api/get', (req, res) => {
+  const data = [
+    { id: '0', title: 'content1', description: 'description of content1' },
+    { id: '1', title: 'content2', description: 'description of content2' },
+    { id: '2', title: 'content3', description: 'description of content3' },
+    { id: '3', title: 'content4', description: 'description of content4' },
+    { id: '4', title: 'content5', description: 'description of content6' },
+  ];
+  res.status(200).json({ success: true, data });
+});
+
+r.post('/mock/api/post', (req, res) => {
+  const body = req.body;
+  res.status(200).json({ success: true, body });
+});
+
 module.exports = r;
