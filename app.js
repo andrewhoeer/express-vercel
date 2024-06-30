a271d8dba2476f6fe41609caeadec5efe2695e5b
EER Team <eer.engine.22@gmail.com>
2024-06-30 21:41:54 +0700

    Update cors

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
+app.use(cors({
+  origin: '*', // You can specify certain origins instead of '*'
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
